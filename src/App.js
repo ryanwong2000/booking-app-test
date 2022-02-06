21f3fb2b12ff488f54c111d4c6d454b14271ea2c
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-02-06 18:47:44 -0500

    toggle forms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
 import MyForm from './components/MyForm';
 import Heading from './components/Heading';
 import SillyForm from './components/SillyForm';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Heading />
-      <MyForm />
-      <SillyForm />
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    silly: false
+  };
+
+  goSilly = () => {
+    this.setState({ silly: !this.state.silly });
+  };
+
+  render() {
+    return (
+      <div>
+        <Heading />
+        {this.state.silly ? <SillyForm /> : <MyForm />}
+        <div style={{ textAlign: 'center' }}>
+          <button type="button" onClick={this.goSilly}>
+            {this.state.silly ? 'the boring one 😴' : 'Go silly 😜'}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
